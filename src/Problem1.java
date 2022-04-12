b2fb3e669bd6f6a65d078c30fe5e4b10bcad6cdd
Artem Bordiuzha <halp3ars@gmail.com>
2022-04-12 10:27:18 +0300

    Igor ne nado

--- src/Problem1.java
+++ src/Problem1.java
@@ -2,49 +2,6 @@ import java.util.HashSet;
 
 public class Problem1 {
 
-    /* public static boolean isValid(char[][] board) {
-
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) // check columns
-            {
-                if (board[i][j] == '.') {
-                    continue;
-                } else if (board[i][j] == '1' | board[i][j] == '2' | board[i][j] == '3' | board[i][j] == '4' | board[i][j] == '5' | board[i][j] == '6' | board[i][j] == '7' | board[i][j] == '8' | board[i][j] == '9') {
-                    for (int k = j + 1; k < board[i].length; k++) {
-                        if (board[i][j] == board[i][k]) {
-                            System.out.println("Matrix contains not valid value");
-                            return false;
-                        }
-                    }
-                } else {
-                    System.out.println("Matrix contains not valid value");
-                    return false;
-                }
-
-            }
-            for (int p = 0; p < board.length; p++) // Check rows
-            {
-                if (board[p][i] == '.') {
-                    continue;
-                } else if (board[p][i] == '1' | board[p][i] == '2' | board[p][i] == '3' | board[p][i] == '4' | board[p][i] == '5' | board[p][i] == '6' | board[p][i] == '7' | board[p][i] == '8' | board[p][i] == '9') {
-                    for (int k = p + 1; k < board[p].length; k++)
-                        if (board[p][i] == board[k][i]) {
-                            System.out.println("Matrix contains not valid value");
-                            return false;
-
-                        }
-
-                } else {
-                    System.out.println("Matrix contains not valid value");
-                    return false;
-                }
-
-            }
-        }
-        System.out.println("Matrix is valid");
-        return true;
-    } */
-
     public static boolean Valid(char[][] board){
 
         int temp ;
@@ -58,14 +15,14 @@ public class Problem1 {
         }
         return true;
     }
-
+    // using String HashSet
    public static boolean IsValidHashSetString(char[][] board) {
 
 
        boolean valida = Valid(board);
        if (!valida) return false;
 
-       HashSet<String> set = new HashSet<>(); //using String HashSet
+       HashSet<String> set = new HashSet<>();
 
            for (int i = 0; i < 9; i++) {
                for (int j = 0; j < 9; j++) {
@@ -78,13 +35,13 @@ public class Problem1 {
    }
 
 
-
+    // using Integer HashSet
     public static boolean IsValidHashSetInteger(char[][] board) {
 
         boolean valida = Valid(board);
         if (!valida) return false;
 
-        HashSet<Integer> set = new HashSet<>(); // using Integer HashSet
+        HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] != '.'){
