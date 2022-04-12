f5e9f1627b7b6191b575bac863eeb0936c600f07
Artem Bordiuzha <halp3ars@gmail.com>
2022-04-12 14:12:38 +0300

    Hash set without valid check method

--- src/Problem1.java
+++ src/Problem1.java
@@ -2,68 +2,46 @@ import java.util.HashSet;
 
 public class Problem1 {
 
-    public static boolean Valid(char[][] board){
-
-        int temp ;
-        for (char[] chars : board) {
-            for (int j = 0; j < board.length; j++) {
-                if (chars[j] != '.') {
-                    temp = Character.getNumericValue(chars[j]);
-                    if (temp == -1 | temp > 9 | temp < 1) return false;
-                }
-            }
-        }
-        return true;
-    }
-    // using String HashSet
-   public static boolean IsValidHashSetString(char[][] board) {
-
-
-       boolean valida = Valid(board);
-       if (!valida) return false;
-
-       HashSet<String> set = new HashSet<>();
-
-           for (int i = 0; i < 9; i++) {
-               for (int j = 0; j < 9; j++) {
-                   if (board[i][j] != '.') {
-                       if (!set.add("col" + j + board[i][j]) || !set.add("row" + i + board[i][j])) return false;
-                   }
-               }
-           }
-       return true;
-   }
-
 
     // using Integer HashSet
     public static boolean IsValidHashSetInteger(char[][] board) {
 
-        boolean valida = Valid(board);
-        if (!valida) return false;
 
-        HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < 9; i++) {
+            int temp;
+            HashSet<Integer> columns = new HashSet<>();
+            HashSet<Integer> rows = new HashSet<>();
             for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.'){
-                    if (!set.add(i * 10 + board[i][j]) || !set.add(100 + j * 10 + board[i][j])) return false;
-                }
+                    temp = Character.getNumericValue(board[i][j]);
+                    if(board[i][j] != '.' && temp < 1 || temp > 9 ) return false;
+                    if(board[j][i] != '.') {
+                        if (!columns.add(i + board[j][i])) return false;
+                    }
+                    if(board[i][j] != '.') {
+                        if (!rows.add(i + board[i][j])) return false;
+                    }
+
+
             }
+
         }
         return true;
     }
+
+
 
 
 
     public static void main(String[] args) {
         char[][] board = {
-                {'5', '3', '7', '.', '.', '.', '.', '.', '.'},
-                {'6', '7', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '3', '.', '.', '.', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
                 {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
                 {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
                 {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
                 {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
                 {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
-                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'3', '.', '.', '4', '1', '9', '.', '.', '5'},
                 {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
 
         char[][] board2 = {
@@ -77,5 +55,8 @@ public class Problem1 {
                 {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
                 {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
 
-        System.out.println(IsValidHashSetString(board));
+        long time = System.nanoTime();
+        System.out.println(IsValidHashSetInteger(board));
+        long end = System.nanoTime();
+        System.out.println(end - time);
     }}
