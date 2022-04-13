12b96ee46ec91ce716f353d86625e9bb20751a3e
Artem Bordiuzha <halp3ars@gmail.com>
2022-04-13 12:05:14 +0300

    Refactoring

--- src/Problem1.java
+++ src/Problem1.java
@@ -2,28 +2,21 @@ import java.util.HashSet;
 
 class Problem1 {
 
-    public  boolean notPoint(char symbol ){
-        return symbol != '.';
-    }
-    public  boolean areConditionCorrect(char symbol){
-        return symbol < '1' || symbol > '9';
-    }
-
-    // using Integer HashSet
-    public  boolean validHashSet(char[][] board) {
+    public  boolean validateMatrix(char[][] board) {
         for (int row = 0; row < 9; row++) {
             HashSet<Character> columns = new HashSet<>();
             HashSet<Character> rows = new HashSet<>();
             for (int col = 0; col < 9; col++) {
-                    if (notPoint(board[col][row]) && !columns.add(board[col][row]) | areConditionCorrect(board[col][row])) return false;
-                    if (notPoint(board[row][col]) && !rows.add(board[row][col]) | areConditionCorrect(board[row][col])) return false;
+                    if (isNotValidElement(board[col][row]) && !columns.add(board[col][row]) ) return false;
+                    if (isNotValidElement(board[row][col]) && !rows.add(board[row][col]) ) return false;
             }
         }
-
         return true;
     }
 
-
-
-
+    private boolean isNotValidElement(char symbol){
+        boolean isNotPoint = symbol != '.';
+        boolean isNotValidDigit = symbol < '1' || symbol > '9';
+        return isNotPoint && isNotValidDigit;
     }
+}
